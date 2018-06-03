cd708ca1165eed2dc19df87ab9a6e95814652848
ZiebaTomasz <ziebatomasz3@gmail.com>
2018-06-03 15:10:08 +0200

    Initial commit.

---
+++ tabata-app/src/main/java/com/tomek/tabataapp/TabataAppApplication.java
@@ -0,0 +1,12 @@
+package com.tomek.tabataapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabataAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TabataAppApplication.class, args);
+  }
+}
